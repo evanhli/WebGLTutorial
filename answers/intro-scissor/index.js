43240475263c486678aab1bf33a69bd283f6d596
Evan Li <evan.li.2010@hotmail.com>
2016-05-23 15:51:46 -0400

    Scissor exercise completed

--- answers/intro-scissor/index.js
+++ answers/intro-scissor/index.js
@@ -14,12 +14,35 @@ exports.draw = function(gl) {
   //Turn on scissor test
   gl.enable(gl.SCISSOR_TEST)
 
-  //Clip out region in center of screen 
-  gl.scissor(w/4, h/4, w/2, h/2)
+  //Clip out region in center of screen
+  gl.scissor(w, h, w, h)
 
   //Clear to white
-  gl.clearColor(1, 1, 1, 1)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.clearColor(1, 1, 1, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //top left
+  gl.scissor(0, h/2, w/2, h/2);
+  gl.clearColor(1, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //top right
+  gl.scissor(w/2, h/2, w/2, h/2);
+  gl.clearColor(0, 1, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //bottom left
+  gl.scissor(0, 0, w/2, h/2);
+  gl.clearColor(0, 0, 1, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //bottom right
+  gl.scissor(w/2, 0, w/2, h/2);
+  gl.clearColor(1, 1, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+
 
   //Turn off scissor test
   gl.disable(gl.SCISSOR_TEST)
